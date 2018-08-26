dd4a23d23c5a8110065e5ec0082f12108f009145
Shaun McGowan <shaun_mcgowan@hotmail.com>
2018-08-26 15:34:30 -0400

    Base rotation garbage added

--- Assets/Scripts/ControllerInputManager.cs
+++ Assets/Scripts/ControllerInputManager.cs
@@ -1,17 +1,68 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 /// <summary>
 /// Handles all the events that are triggered by controller input
 /// </summary>
 public class ControllerInputManager : MonoBehaviour {
 
-	
-	void Start ()
+    private bool isSet = false;
+    private bool goodToRotate = true;
+
+	void LateUpdate ()
     {
-		
-	}
+        if (PositionManager.Instance.rightController && !isSet)
+        {
+            isSet = true;
+            PositionManager.Instance.rightController.GetComponent<VRTK_ControllerEvents>().TouchpadAxisChanged += new ControllerInteractionEventHandler(AxisChanged);
+            PositionManager.Instance.rightController.GetComponent<VRTK_ControllerEvents>().TouchpadTouchEnd += new ControllerInteractionEventHandler(TouchpadReleased);
+
+        }
+
+    }
+
+    /// <summary>
+    /// Lets the player rotate again once the let go of the touchpad
+    /// </summary>
+    private void TouchpadReleased(object sender, ControllerInteractionEventArgs e)
+    {
+        goodToRotate = true;
+    }
+
+
+    private void AxisChanged(object sender, ControllerInteractionEventArgs e)
+    {
+        if (goodToRotate)
+        {
+            if (e.touchpadAxis.x >= 0.9f)
+            {
+                // Rotate Right 90
+                GameObject destination = new GameObject();
+                destination.transform.position = PositionManager.Instance.player.position;
+                destination.transform.rotation = PositionManager.Instance.player.rotation;
+                destination.transform.localEulerAngles = new Vector3(destination.transform.localEulerAngles.x, destination.transform.localEulerAngles.y + 90.0f, destination.transform.localEulerAngles.z);
+                PositionManager.Instance.teleportPlayer(PositionManager.Instance.player, destination.transform);
+                print("The touchpad axis is :  " + e.touchpadAxis.ToString());
+                GameObject.Destroy(destination);
+                goodToRotate = false;
+
+            }
+            else if (e.touchpadAxis.x <= -0.9f)
+            {
+                // Rotate Left 90
+                GameObject destination = new GameObject();
+                destination.transform.position = PositionManager.Instance.player.position;
+                destination.transform.rotation = PositionManager.Instance.player.rotation;
+                destination.transform.localEulerAngles = new Vector3(destination.transform.localEulerAngles.x, destination.transform.localEulerAngles.y - 90.0f, destination.transform.localEulerAngles.z);
+                PositionManager.Instance.teleportPlayer(PositionManager.Instance.player, destination.transform);
+                GameObject.Destroy(destination);
+                goodToRotate = false;
+            }
+        }
+    }
 
     #region singleton
     public static ControllerInputManager Instance;
--- Assets/Scripts/PositionManager.cs
+++ Assets/Scripts/PositionManager.cs
@@ -8,13 +8,17 @@ using VRTK;
 /// </summary>
 public class PositionManager : MonoBehaviour {
 
+    #region PublicVars
     [HideInInspector]
     public Transform player;
-
     [HideInInspector]
     public Transform rightController;
     [HideInInspector]
     public Transform leftController;
+    [HideInInspector]
+    public GameObject playAreaScripts;
+    #endregion
+
 
     private void LateUpdate()
     {
@@ -23,6 +27,7 @@ public class PositionManager : MonoBehaviour {
             player = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
             leftController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
             rightController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+            playAreaScripts = GameObject.Find("PlayArea");
         }
     }
     /// <summary>
@@ -31,8 +36,8 @@ public class PositionManager : MonoBehaviour {
     /// <param name="player">The transform of the player object</param>
     /// <param name="destination">The transform of the destination for the player</param>
     public void teleportPlayer(Transform player, Transform destination)
-   {
-       
+    {
+
         Transform playArea = VRTK_DeviceFinder.PlayAreaTransform();
         Transform oldParent = playArea.parent;
         GameObject uselessEmpty = new GameObject("UselessEmpty"); //Used to rotate the player
@@ -40,12 +45,12 @@ public class PositionManager : MonoBehaviour {
         playArea.parent = uselessEmpty.transform; // Parenting the play area so it can be rotated
 
         // Rotating the player to the correct pos
+        StartCoroutine(fadeToBlack());
         uselessEmpty.transform.position = destination.position;
         uselessEmpty.transform.localEulerAngles = new Vector3(playArea.localEulerAngles.x, destination.localEulerAngles.y, playArea.localEulerAngles.z);
 
         // removing the useless empty as a parent and reatching to the old one
         playArea.parent = oldParent;
-        
 
         // Calculating the offset of the player from the destination position
         Vector3 offset = player.position - destination.position;
@@ -54,9 +59,23 @@ public class PositionManager : MonoBehaviour {
         // Removing the empty we created for the player
         GameObject.Destroy(uselessEmpty);
 
-        
+    }
 
-   }
+
+    public void rotatePlayerLeft()
+    {
+
+    }
+    private IEnumerator fadeToBlack()
+    {
+        float fadeTime = 0.3f;
+        playAreaScripts.GetComponent<VRTK_HeadsetFade>().Fade(Color.black,fadeTime);
+        yield return new WaitForSeconds(fadeTime);
+
+        playAreaScripts.GetComponent<VRTK_HeadsetFade>().Unfade(fadeTime);
+
+        yield return null;
+    }
 
     #region singleton
     public static PositionManager Instance;

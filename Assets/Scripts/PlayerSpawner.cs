9217b39176829f14be907f51432b991e6dbd8e3b
Shaun McGowan <shaun_mcgowan@hotmail.com>
2018-08-26 14:25:06 -0400

    Added the teleport player function

---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Spawns the player at the position of the object script is attatched to
+/// </summary>
+public class PlayerSpawner : MonoBehaviour {
+
+	
+	void Update ()
+    {
+		if(PositionManager.Instance.player != null)
+        {
+            PositionManager.Instance.teleportPlayer(PositionManager.Instance.player, this.transform);
+            Destroy(this.gameObject);
+        }
+	}
+	
+	
+}
---
+++ Assets/Scripts/PositionManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+/// <summary>
+/// Handles controller the position of player
+/// </summary>
+public class PositionManager : MonoBehaviour {
+
+    [HideInInspector]
+    public Transform player;
+
+    [HideInInspector]
+    public Transform rightController;
+    [HideInInspector]
+    public Transform leftController;
+
+    private void LateUpdate()
+    {
+        if (player == null)
+        {
+            player = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+            leftController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
+            rightController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+        }
+    }
+    /// <summary>
+    /// Deals with teleporting the player correctly based of the camera pos not the player area pos
+    /// </summary>
+    /// <param name="player">The transform of the player object</param>
+    /// <param name="destination">The transform of the destination for the player</param>
+    public void teleportPlayer(Transform player, Transform destination)
+   {
+       
+        Transform playArea = VRTK_DeviceFinder.PlayAreaTransform();
+        Transform oldParent = playArea.parent;
+        GameObject uselessEmpty = new GameObject("UselessEmpty"); //Used to rotate the player
+
+        playArea.parent = uselessEmpty.transform; // Parenting the play area so it can be rotated
+
+        // Rotating the player to the correct pos
+        uselessEmpty.transform.position = destination.position;
+        uselessEmpty.transform.localEulerAngles = new Vector3(playArea.localEulerAngles.x, destination.localEulerAngles.y, playArea.localEulerAngles.z);
+
+        // removing the useless empty as a parent and reatching to the old one
+        playArea.parent = oldParent;
+        
+
+        // Calculating the offset of the player from the destination position
+        Vector3 offset = player.position - destination.position;
+        playArea.position = playArea.position - offset;
+
+        // Removing the empty we created for the player
+        GameObject.Destroy(uselessEmpty);
+
+        
+
+   }
+
+    #region singleton
+    public static PositionManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    #endregion
+}

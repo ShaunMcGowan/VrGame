6390a126822de4fb00467d455c7ff6550015b992
Shaun McGowan <shaun_mcgowan@hotmail.com>
2018-08-26 13:51:17 -0400

    Imported the bases for some stuff and started implementing the controls

---
+++ Assets/ControllerInputManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handles all the events that are triggered by controller input
+/// </summary>
+public class ControllerInputManager : MonoBehaviour {
+
+	
+	void Start ()
+    {
+		
+	}
+
+    #region singleton
+    public static ControllerInputManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    private void OnDestroy()
+    {
+        if (Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    #endregion
+}
---
+++ Assets/PositionManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+/// <summary>
+/// Handles controller the position of player
+/// </summary>
+public class PositionManager : MonoBehaviour {
+
+    [HideInInspector]
+    public Transform player;
+
+    [HideInInspector]
+    public Transform rightController;
+    [HideInInspector]
+    public Transform leftController;
+    void Start ()
+    {
+        player = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.Headset);
+        leftController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.LeftController);
+        rightController = VRTK_DeviceFinder.DeviceTransform(VRTK_DeviceFinder.Devices.RightController);
+    }
+	
+    /// <summary>
+    /// Deals with teleporting the player correctly based of the camera pos not the player area pos
+    /// </summary>
+    /// <param name="player">The transform of the player object</param>
+    /// <param name="destination">The transform of the destination for the player</param>
+   public void teleportPlayer(Transform player, Transform destination)
+   {
+
+   }
+
+    #region singleton
+    public static PositionManager Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+    }
+    private void OnDestroy()
+    {
+        if(Instance == this)
+        {
+            Instance = null;
+        }
+    }
+    #endregion
+}

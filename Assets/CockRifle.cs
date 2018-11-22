635ec0a758c32d427c80b96bafcde9bb40a4d150
Shaun McGowan <shaun_mcgowan@hotmail.com>
2018-11-21 21:47:27 -0500

    Over it

---
+++ Assets/CockRifle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Script is responsible for handling the cocking of a rifle after it is fired
+/// </summary>
+public class CockRifle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        print("The current eurler angle of the bolt is " + transform.localEulerAngles.ToString());
+	}
+}
